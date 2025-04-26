ad5b3e4120a3a5315e55bdc16ce7502b7a97a4fd
jto <jan@ostendorf.com>
2025-04-26 15:48:31 +0200

    save

--- code/solve_Lindblad_eq.py
+++ code/solve_Lindblad_eq.py
@@ -1,56 +1,38 @@
-from qutip import *
+import qutip as qp
 import numpy as np
 
 # Define the parameters
-wc = 1.0  # Cavity frequency
-wq = 1.5  # Qubit frequency
+omega_c = 1.0  # Cavity frequency
+omega_q = 1.5  # Qubit frequency
 g = 0.1   # Coupling strength
-ed = 0.2  # Driving amplitude for cavity
-ef = 0.3  # Driving amplitude for qubit
-wd = 1.1  # Driving frequency for cavity
-wf = 1.6  # Driving frequency for qubit
+epsilon_d = 0.2  # Driving amplitude for cavity
+epsilon_r = 0.3  # Driving amplitude for qubit
+omega_d = 1.1  # Driving frequency for cavity
+omega_r = 1.6  # Driving frequency for qubit
 
 # Define the operators
-a = destroy(2)  # Cavity annihilation operator (assuming 2 levels for the cavity for simplicity, can be larger)
-sm = destroy(2) # Qubit lowering operator
-sz = sigmaz()   # Qubit Z operator
-sp = create(2)  # Qubit raising operator
+a = qp.destroy(2)  # Cavity annihilation operator (assuming 2 levels for the cavity for simplicity, can be larger)
+sigma_minus = qp.destroy(2) # Qubit lowering operator
+sigma_plus = qp.create(2)  # Qubit raising operator
 
 # Define the Hamiltonian terms
-term1 = wc * a.dag() * a
-term2 = -0.5 * wq * sz
-term3 = g * (a.dag() * sm + a * sp)
-term4_coeff_d = ed
-term4_op_d = a.dag()
-term4_coeff_d_conj = np.conjugate(ed)
-term4_op_d_dag = a
-term5_coeff_f = ef
-term5_op_f = sp
-term5_coeff_f_conj = np.conjugate(ef)
-term5_op_f_dag = sm
+term1 = omega_c * a.dag() * a
+term2 = -1/2 * omega_q * qp.sigmaz()
+term3 = g * (a.dag() * sigma_minus + a * sigma_plus)
 
 # Define the time-dependent coefficients as strings for QuTiP's t-dependence
-def coeff_d(t, args):
-    return ed * np.exp(-1j * wd * t)
+def term4_coeff(t, args):
+    coeff_d = args['epsilon_d'] * np.exp(-1j * args['omega_d'] * t)
+    coeff_r = args['epsilon_r'] * np.exp(-1j * args['omega_r'] * t)
+    return coeff_d + coeff_r
 
-def coeff_d_conj(t, args):
-    return np.conjugate(ed) * np.exp(1j * wd * t)
+def term4_coeff_conj(t, args):
+    return np.conjugate(term4_coeff(t, args))
 
-def coeff_f(t, args):
-    return ef * np.exp(-1j * wf * t)
-
-def coeff_f_conj(t, args):
-    return np.conjugate(ef) * np.exp(1j * wf * t)
+args = dict(epsilon_d=epsilon_d, omega_d=omega_d, epsilon_r=epsilon_r, omega_r=omega_r)
 
 # Combine the terms into a list for the time-dependent Hamiltonian
-H = [
-    term1,
-    term2,
-    term3,
-    [term4_op_d, coeff_d],
-    [term4_op_d, coeff_d].dag(),
-    [term5_op_f, coeff_f],
-    [term5_op_f, coeff_f].dag()
-]
+H = [term1, term2, term3, [a.dag(), term4_coeff], [a, term4_coeff_conj]]
 
-print(H)
+# Create the time-dependent Hamiltonian onject
+H_t = qp.QobjEvo(H, args=args)
